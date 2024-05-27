5335c027a2aef8b132e80df64cbd9e268712e226
MarcoFacchin <marco.facchin@vdabcampus.be>
2024-05-27 14:51:05 +0200

    GebruikerController aangepast: -file aanmaken

--- src/main/java/be/vdab/landen/GebruikerController.java
+++ src/main/java/be/vdab/landen/GebruikerController.java
@@ -3,12 +3,20 @@ package be.vdab.landen;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @RestController
 @RequestMapping("/")
 public class GebruikerController {
-    GebruikerController() {
-        System.out.println("Besturingssysteem: " + System.getProperty("os.name"));
-        System.out.println("Gebruikersnaam: " + System.getProperty("user.name"));
-        System.out.println("Home directory van de gebruiker: " + System.getProperty("user.home"));
+    GebruikerController() throws IOException {
+        var homeDirectory = Path.of(System.getProperty("user.home"));
+        var bestand = homeDirectory.resolve("organisatie.txt");
+        if (!Files.exists(bestand)) {
+            System.out.println("------->");
+            Files.writeString(bestand, "VDAB");
+            System.out.println(bestand + " gemaakt");
+        }
     }
 }
